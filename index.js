de8a3699bd0857ceb123fd4aa7f11eecf2285439
Dhanushka <dhanushkatharindu99@gmail.com>
2022-06-19 00:21:04 +0530

    completed project

--- index.js
+++ index.js
@@ -1,20 +1,43 @@
-var express = require('express');
-var cors = require('cors');
-require('dotenv').config()
+var express = require("express");
+var cors = require("cors");
+require("dotenv").config();
+const multer = require("multer");
+const fs = require("fs/promises");
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, "./tmp");
+    },
+    filename: function (req, file, cb) {
+        const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+        cb(null, file.fieldname + "-" + uniqueSuffix);
+    },
+});
+
+const upload = multer({ storage });
 
 var app = express();
 
 app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use("/public", express.static(process.cwd() + "/public"));
 
-app.get('/', function (req, res) {
-    res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", function (req, res) {
+    res.sendFile(process.cwd() + "/views/index.html");
 });
 
+app.post("/api/fileanalyse", upload.single("upfile"), async (req, res) => {
+    res.json({
+        name: req.file.originalname,
+        type: req.file.mimetype,
+        size: req.file.size,
+    });
 
-
+    await fs.unlink(`./tmp/${req.file.filename}`, (err) => {
+        console.error(err);
+    });
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+    console.log("Your app is listening on port " + port);
 });
